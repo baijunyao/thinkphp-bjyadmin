45cb7cf9a69b2fdff011340fe42cdbf23df65cc3
baijunyao <baijunyao@baijunyao.com>
2016-06-09 08:47:15 +0800

    tcpdf整合完成

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1069,3 +1069,59 @@ function get_device_type(){
     }
     return $type;
 }
+
+/**
+ * 生成pdf
+ * @param  string $html      需要生成的内容
+ */
+function pdf($html='<h1>1111</h1>'){
+    vendor('Tcpdf.tcpdf');
+    // p($html);die;
+    $pdf = new \Tcpdf(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    // 设置打印模式
+    $pdf->SetCreator(PDF_CREATOR);
+    $pdf->SetAuthor('Nicola Asuni');
+    $pdf->SetTitle('TCPDF Example 001');
+    $pdf->SetSubject('TCPDF Tutorial');
+    $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+    // 是否显示页眉
+    $pdf->setPrintHeader(false);
+    // 设置页眉显示的内容
+    $pdf->SetHeaderData('logo.png', 60, 'baijunyao.com', '白俊遥博客', array(0,64,255), array(0,64,128));
+    // 设置页眉字体
+    $pdf->setHeaderFont(Array('dejavusans', '', '12'));
+    // 页眉距离顶部的距离
+    $pdf->SetHeaderMargin('5');
+    // 是否显示页脚
+    $pdf->setPrintFooter(true);
+    // 设置页脚显示的内容
+    $pdf->setFooterData(array(0,64,0), array(0,64,128));
+    // 设置页脚的字体
+    $pdf->setFooterFont(Array('dejavusans', '', '10'));
+    // 设置页脚距离底部的距离
+    $pdf->SetFooterMargin('10');
+    // 设置默认等宽字体
+    $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+    // 设置行高
+    $pdf->setCellHeightRatio(1);
+    // 设置左、上、右的间距
+    $pdf->SetMargins('10', '10', '10');
+    // 设置是否自动分页  距离底部多少距离时分页
+    $pdf->SetAutoPageBreak(TRUE, '15');
+    // 设置图像比例因子
+    $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+    if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+        require_once(dirname(__FILE__).'/lang/eng.php');
+        $pdf->setLanguageArray($l);
+    }
+    $pdf->setFontSubsetting(true);
+    $pdf->SetFont('dejavusans', '', 14, '', true);
+    $pdf->AddPage();
+    // $pdf->setTextShadow(array('enabled'=>false, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+    // 设置字体
+    $pdf->SetFont('dejavusans','', 20);
+    $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+    $pdf->Output('example_001.pdf', 'I');
+}
+
+
---
+++ ThinkPHP/Library/Vendor/Tcpdf/index.php
@@ -0,0 +1,106 @@
+<?php
+//============================================================+
+// File name   : example_001.php
+// Begin       : 2008-03-04
+// Last Update : 2013-05-14
+//
+// Description : Example 001 for TCPDF class
+//               Default Header and Footer
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.sucaihuo.com
+//               info@tecnick.com
+//============================================================+
+
+/**
+ * Creates an example PDF TEST document using TCPDF
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: Default Header and Footer
+ * @author Nicola Asuni
+ * @since 2008-03-04
+ */
+
+// Include the main TCPDF library (search for installation path).
+require_once('tcpdf.php');
+
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Nicola Asuni');
+$pdf->SetTitle('TCPDF Example 001');
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
+$pdf->setFooterData(array(0,64,0), array(0,64,128));
+
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set some language-dependent strings (optional)
+if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+	require_once(dirname(__FILE__).'/lang/eng.php');
+	$pdf->setLanguageArray($l);
+}
+
+// ---------------------------------------------------------
+
+// set default font subsetting mode
+$pdf->setFontSubsetting(true);
+
+// Set font
+// dejavusans is a UTF-8 Unicode font, if you only need to
+// print standard ASCII chars, you can use core fonts like
+// helvetica or times to reduce file size.
+$pdf->SetFont('dejavusans', '', 14, '', true);
+
+// Add a page
+// This method has several options, check the source code documentation for more information.
+$pdf->AddPage();
+
+// set text shadow effect
+$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+
+// Set some content to print
+$html = <<<EOD
+<h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
+<i>素材火</i>
+<p>提供网站模板,网站源码,网页图标,网页特效,中文字体等网页设计素材下载,为广大网友制作网页提供网站素材免费下载参考.</p>
+<p>素材火网站是致力于网页前端开发用户的一个下载资源交流平台网站。在这里您可以找到时下最流行、最新进的网页前端脚本代码和网页设计图片素材。且融入了目前最流行微博客社交功能，您可以在这找到与您一起的同行工作者。您可以在这里发布您自己的代码和图片等素材，与别人分享。同时您也可以申请需要的素材。</p>
+<p style="color:#CC0000;">Welcome to <a href="http://www.sucaihuo.com">素材火</a></p>
+EOD;
+$pdf->SetFont('stsongstdlight','', 20);
+// Print text using writeHTMLCell()
+$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
+// ---------------------------------------------------------
+
+// Close and output PDF document
+// This method has several options, check the source code documentation for more information.
+$pdf->Output('example_001.pdf', 'I');
+
+//============================================================+
+// END OF FILE
+//============================================================+
