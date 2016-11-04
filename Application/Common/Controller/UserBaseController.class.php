a663e7e791ff4f7b8fd7d511f8dba98fb7077073
baijunyao <baijunyao@baijunyao.com>
2016-11-05 00:05:48 +0800

    完善User中允许登录的连接判断

--- Application/Common/Controller/UserBaseController.class.php
+++ Application/Common/Controller/UserBaseController.class.php
@@ -10,15 +10,16 @@ class UserBaseController extends BaseController{
 	 */
 	public function _initialize(){
 		parent::_initialize();
-       // 添加不需要验证是否登录的连接
+        // 添加不需要验证是否登录的连接 全部小写
         $not_need_login=array(
-            'User/Goods/app_buy'
+            'user/goods/app_buy'
             );
-        $action=trim(__ACTION__,'/');
+        // 转小写以兼容url大小写不统一的问题
+        $action=strtolower(trim(__ACTION__,'/'));
         if (!in_array($action, $not_need_login)) {
             // 检测是否登录
             if (!check_login()) {
-                if (IS_APP || IS_AJAX) {
+                if (IS_AJAX) {
                     ajax_return('','您需要登录！',1);
                 }else{
                     $this->error('您需要登录！');
