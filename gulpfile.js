435bd089fc6f8db6f9cb3afd9daea3f137fa42f1
baijunyao <baijunyao@baijunyao.com>
2017-01-02 14:57:34 +0800

    开始使用gulp

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    minifyCss   = require('gulp-minify-css'),
+    plumber     = require('gulp-plumber'),
+    babel       = require('gulp-babel'),
+    uglify      = require('gulp-uglify'),
+    clearnHtml  = require("gulp-cleanhtml");
+    imagemin    = require('gulp-imagemin');
+    browserSync = require('browser-sync').create(),
+    reload      = browserSync.reload;
+    
+// 定义源代码的目录和编译压缩后的目录
+var src='tpl_src',
+    dist='tpl';
+
+// 编译全部scss 并压缩
+gulp.task('css', function(){
+    gulp.src(src+'/**/*.scss')
+        .pipe(sass())
+        .pipe(minifyCss())
+        .pipe(gulp.dest(dist))
+})
+
+// 编译全部js 并压缩
+gulp.task('js', function() {
+  gulp.src(src+'/**/*.js')
+    .pipe(plumber())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(dist));
+});
+
+// 压缩全部html
+gulp.task('html', function () {
+    gulp.src(src+'/**/*.html')
+    .pipe(clearnHtml())
+    .pipe(gulp.dest(dist));
+});
+
+// 压缩全部image
+gulp.task('image', function () {
+    gulp.src([src+'/**/*.+(jpg|jpeg|png|gif|bmp)'])
+    .pipe(imagemin())
+    .pipe(gulp.dest(dist));
+});
+
+// 自动刷新
+gulp.task('server', function() {
+    browserSync.init({
+        proxy: "tbjyblog.com", // 指定代理url
+        notify: false, // 刷新不弹出提示
+    });
+    // 监听scss文件编译
+    gulp.watch(src+'/**/*.scss', ['css']);   
+
+    // 监听html文件变化后刷新页面
+    gulp.watch(src+"/**/*.js", ['js']).on("change", reload);
+
+    // 监听html文件变化后刷新页面
+    gulp.watch(src+"/**/*.html", ['html']).on("change", reload);
+
+    // 监听css文件变化后刷新页面
+    gulp.watch(dist+"/**/*.css").on("change", reload);
+});
+
+// 监听事件
+gulp.task('default', ['css', 'js', 'image', 'html', 'server'])
